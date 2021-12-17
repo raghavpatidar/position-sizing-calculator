07a6c9a4a70df2c89703b887cae2f45bdee4d745
LENOVO <raghavlalapatidar@gmail.com>
2021-12-17 19:05:38 +0530

    autocomplete removed

---
+++ app.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    let button = document.querySelector("#btn");
+
+    // Function for calculating BMI
+    button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+    console.log('kiong is here')
+    let cp = parseFloat(document
+        .querySelector("#cp").value);
+    let slp = parseFloat(document
+        .querySelector("#slp").value);
+    let account = parseFloat(document.querySelector('#account').value);
+    let risk = parseFloat(document.querySelector('#risk').value);
+    let percent = (account * risk) / 100;
+    let q = percent / (Math.abs(cp - slp));
+    console.log(q);
+    let result = document.querySelector("#result");
+    result.innerHTML = `lee dhek le teri aukatt${q}`;
+}
